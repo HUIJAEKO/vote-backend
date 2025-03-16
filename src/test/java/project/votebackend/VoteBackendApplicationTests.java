ca3f432449da163b58ffd3dc8be8845fc8e7332c
HUIJAEKO <xrhgmlwox@naver.com>
2025-03-16 16:20:56 +0900

    first commit

---
+++ src/test/java/project/votebackend/VoteBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package project.votebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoteBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
