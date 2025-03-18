3d3d2ac8c126b5c27d6c2e3325188d0ba11fccff
HUIJAEKO <xrhgmlwox@naver.com>
2025-03-18 17:33:20 +0900

    refactor: BaseEntity 추가
    추후에 다른 도메인에서도 사용할 생성 시간, 수정 시간을 BaseEntity 추상 클래스로 생성하였습니다.

--- src/main/java/project/votebackend/domain/User.java
+++ src/main/java/project/votebackend/domain/User.java
@@ -2,24 +2,18 @@ package project.votebackend.domain;
 
 import jakarta.persistence.*;
 import lombok.*;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-import project.votebackend.config.JpaAuditingConfiguration;
 import project.votebackend.type.Gender;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 
 @Entity
-@EntityListeners(AuditingEntityListener.class)
 @Table(name = "users")
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class User {
+public class User extends BaseEntity{
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -55,12 +49,4 @@ public class User {
 
     @Column(nullable = false)
     private LocalDate birthdate;
-
-    @Column(nullable = false)
-    @CreatedDate
-    private LocalDateTime createdAt;
-
-    @Column(nullable = false)
-    @LastModifiedDate
-    private LocalDateTime updatedAt;
 }
