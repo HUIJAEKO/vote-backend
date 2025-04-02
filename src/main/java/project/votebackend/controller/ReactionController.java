f64e490be557c6a20b906d4b00df6fdb01b0409e
HUIJAEKO <xrhgmlwox@naver.com>
2025-04-02 09:16:13 +0900

    feat: 좋아요, 북마크 처리
    좋아요 및 북마크 기능 추가하였습니다.

---
+++ src/main/java/project/votebackend/controller/ReactionController.java
@@ -0,0 +1,40 @@
+package project.votebackend.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import project.votebackend.security.CustumUserDetails;
+import project.votebackend.service.ReactionService;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/reaction")
+public class ReactionController {
+
+    private final ReactionService reactionService;
+
+    //좋아요 처리
+    @PostMapping("/like")
+    public ResponseEntity<?> like(
+            @RequestParam Long voteId,
+            @AuthenticationPrincipal CustumUserDetails userDetails
+    ) {
+        reactionService.like(voteId, userDetails.getId());
+        return ResponseEntity.ok("success");
+    }
+
+    //북마크 처리
+    @PostMapping("/bookmark")
+    public ResponseEntity<?> bookmark(
+            @RequestParam Long voteId,
+            @AuthenticationPrincipal CustumUserDetails userDetails
+    ) {
+        reactionService.bookmark(voteId, userDetails.getId());
+        return ResponseEntity.ok("success");
+    }
+
+}
