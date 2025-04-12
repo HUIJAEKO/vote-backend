c238e6851d8299df3fbdb9e62b55bafd96a7c436
HUIJAEKO <xrhgmlwox@naver.com>
2025-04-12 12:58:35 +0900

    feat: 다른 사용자의 프로필 조회
    다른 사용자의 프로필을 조회하는 api작성하였습니다.

--- src/main/java/project/votebackend/controller/UserController.java
+++ src/main/java/project/votebackend/controller/UserController.java
@@ -1,12 +1,13 @@
 package project.votebackend.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
-import project.votebackend.dto.MyPageDto;
+import project.votebackend.dto.UserPageDto;
 import project.votebackend.security.CustumUserDetails;
 import project.votebackend.service.UserService;
 
@@ -19,11 +20,22 @@ public class UserController {
 
     //마이페이지 조회
     @GetMapping("/mypage")
-    public ResponseEntity<MyPageDto> getMyPage(
+    public ResponseEntity<UserPageDto> getMyPage(
             @AuthenticationPrincipal CustumUserDetails userDetails,
             @PageableDefault(size = 10) Pageable pageable
     ) {
-        MyPageDto dto = userService.getMyPage(userDetails.getId(), pageable);
+        UserPageDto dto = userService.getMyPage(userDetails.getId(), pageable);
         return ResponseEntity.ok(dto);
+    }
+
+    //다른 사용자 조회
+    @GetMapping("/{userId}")
+    public ResponseEntity<UserPageDto> getUserPage(
+            @PathVariable Long userId,
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size
+    ) {
+        UserPageDto userPage = userService.getUserPage(userId, PageRequest.of(page, size));
+        return ResponseEntity.ok(userPage);
     }
 }
