8fe09c6e07c4a760906ffcffd2f9d6eda1ff7b13
HUIJAEKO <xrhgmlwox@naver.com>
2025-04-12 21:46:44 +0900

    fix: 사용자 팔로우, 언팔로우 오류 해결
    사용자 팔로우 및 언팔로우 시 userId가 제대로 넘어가지 않는 문제 해결, 사용자 조회 시 팔로우 여부 확인

--- src/main/java/project/votebackend/config/SecurityConfig.java
+++ src/main/java/project/votebackend/config/SecurityConfig.java
@@ -25,7 +25,7 @@ public class SecurityConfig {
             .authorizeHttpRequests(auth -> auth
                 .requestMatchers("/auth/**", "/image/upload").permitAll() // 로그인 & 업로드 허용
                 .requestMatchers("/vote/**", "/reaction/**", "/storage/**", "user/**",
-                        "comment/**", "comment-like/**", "search/**").authenticated() // 나머지는 인증 필요
+                        "comment/**", "comment-like/**", "search/**", "follow/**").authenticated() // 나머지는 인증 필요
                 .anyRequest().permitAll()
             )
 
