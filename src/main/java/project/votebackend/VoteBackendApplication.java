0495ea7d458f2893615e05b3199ef43a994683c3
HUIJAEKO <xrhgmlwox@naver.com>
2025-04-27 13:49:04 +0900

    fix: 페이징 처리 경고 해결
    1. JPA EntityGraph + 페이징을 같이 쓰면 성능이 나빠져, fetch join삭제.
    2. 컨트롤러에서 Map<String, Object> 형태로 응답 포장하 PageImpl 직렬화 경고 해결

--- src/main/java/project/votebackend/VoteBackendApplication.java
+++ src/main/java/project/votebackend/VoteBackendApplication.java
@@ -2,8 +2,10 @@ package project.votebackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
+@EnableSpringDataWebSupport
 public class VoteBackendApplication {
 
     public static void main(String[] args) {
