f11409aac527a2ac91ed6129c35578165c248284
HUIJAEKO <xrhgmlwox@naver.com>
2025-05-09 15:27:17 +0900

    chore: aws config

--- src/main/java/project/votebackend/repository/SearchRepository.java
+++
@@ -1,9 +0,0 @@
-package project.votebackend.repository;
-
-import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
-import org.springframework.stereotype.Repository;
-import project.votebackend.elasticSearch.VoteDocument;
-
-@Repository
-public interface SearchRepository extends ElasticsearchRepository<VoteDocument, String> {
-}
