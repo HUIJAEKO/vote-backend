e12fe930b855615e3829295b127557d1d922d48b
HUIJAEKO <xrhgmlwox@naver.com>
2025-05-14 14:25:16 +0900

    refactor: 메인페이지 쿼리 최적화

--- src/main/java/project/votebackend/repository/CommentRepository.java
+++ src/main/java/project/votebackend/repository/CommentRepository.java
@@ -3,6 +3,8 @@ package project.votebackend.repository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import project.votebackend.domain.Comment;
 
@@ -15,5 +17,15 @@ public interface CommentRepository extends JpaRepository<Comment, Long> {
 
     //자식 댓글
     List<Comment> findByParent_CommentIdOrderByCreatedAtAsc(Long parentId);
+
+    //댓글 수 카운트
+    @Query(value = """
+        SELECT vote_id, COUNT(*) AS comment_count
+        FROM comment
+        WHERE parent_id IS NULL AND vote_id IN :voteIds
+        GROUP BY vote_id
+    """, nativeQuery = true)
+    List<Object[]> countParentCommentsByVoteIds(@Param("voteIds") List<Long> voteIds);
+
 
 }
